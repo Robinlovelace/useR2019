9758a567d353c6e7deb455cfc694fe3c02bf6e45
Robin Lovelace <rob00x@gmail.com>
2019-07-09 19:18:22 +0100

    Add figures

--- road-casualty-map.R
+++ road-casualty-map.R
@@ -8,18 +8,17 @@ df1 = WHO::get_data("RS_198")
 
 library(tmap)
 data("World")
-library(sp)
 plot(World)
 names(World)
 head(World$name)
 library(dplyr)
 df1 = rename(df1, name = country)
-World@data = left_join(World@data, df1)
-World@data$name[is.na(World$value)]
+World = left_join(World, df1)
+World$name[is.na(World$value)]
 tm_shape(shp = World) + tm_fill("value")
 
 # not all have data - refactor names - rename...
-World@data$name[is.na(World$value)]
+World$name[is.na(World$value)]
 df1$name[! df1$name %in% World$name] # mismatched names
 
 cname = "Russia"
@@ -42,11 +41,11 @@ df1$name[grep(cname, df1$name)] = World$name[grep(cname, World$name)]
 df1$name[! df1$name %in% World$name]
 
 data("World")
-World@data = left_join(World@data, df1)
-World@data$name[is.na(World$value)]
+World = left_join(World, df1)
+World$name[is.na(World$value)]
 tm_shape(shp = World) + tm_fill("value")
 
-World@data = rename(World@data, `Road deaths\nper 100,000` = value)
+World = rename(World, `Road deaths\nper 100,000` = value)
 
 # plot the result - thanks to tmap: 
 # https://github.com/mtennekes/tmap
@@ -54,9 +53,10 @@ world_eck = tmaptools::set_projection(World, "eck4")
 x = tm_shape(world_eck) +
   tm_borders("grey20") +
   tm_grid(projection="longlat", labels.size = .5, alpha = 0.2) +
-  tm_fill("Road deaths\nper 100,000", n = 4) +
-  tm_text("name", size="AREA")
-x +
-  tm_style_bw()
-tmap::save_tmap(x, "road-casualties.png")
+  tm_fill("Road deaths\nper 100,000", n = 4, palette = "inferno", ) +
+  tm_text("name", size="AREA") +
+  tm_layout(bg.color = "lightblue")
+x 
+# +  tm_style_bw()
+tmap::tmap_save(x, "road-casualties.png")
 tmap::save_tmap(x, "road-casualties.svg")
